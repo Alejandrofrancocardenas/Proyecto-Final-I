de0de59b8bd327140257edc7d8809ab6f5690f28
Alejandrfrancocardenas <alejandro.francoc1@uqvirtual.edu.co>
2025-05-24 23:02:41 -0500

    Avance interfaz

--- SistemaGestionHospital/src/main/java/co/edu/uniquindio/sistemagestionhospital/viewController/MainViewController.java
+++ SistemaGestionHospital/src/main/java/co/edu/uniquindio/sistemagestionhospital/viewController/MainViewController.java
@@ -1,4 +1,34 @@
 package co.edu.uniquindio.sistemagestionhospital.viewController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainViewController {
+
+    @FXML
+    private Button btnPacientes;
+
+    @FXML
+    private Button btnMedicos;
+
+    @FXML
+    private Button btnCerrarSesion;
+
+    @FXML
+    void gestionarPacientes(ActionEvent event) {
+        System.out.println("Abriendo ventana de pacientes...");
+        // Aquí puedes cargar otro FXML si deseas
+    }
+
+    @FXML
+    void gestionarMedicos(ActionEvent event) {
+        System.out.println("Abriendo ventana de médicos...");
+    }
+
+    @FXML
+    void cerrarSesion(ActionEvent event) {
+        System.out.println("Sesión cerrada");
+        System.exit(0); // Cierra la app
+    }
 }
--- SistemaGestionHospital/src/main/java/module-info.java
+++ SistemaGestionHospital/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module co.edu.uniquindio.sistemagestionhospital {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    opens co.edu.uniquindio.sistemagestionhospital.viewController to javafx.fxml;
     opens co.edu.uniquindio.sistemagestionhospital to javafx.fxml;
     exports co.edu.uniquindio.sistemagestionhospital;
     exports co.edu.uniquindio.sistemagestionhospital.Controller;
